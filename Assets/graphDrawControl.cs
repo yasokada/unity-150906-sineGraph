855e2e36f0f92df2de9f4852fc7e703cb793fa28
y_okada <yasokada@gmail.com>
2015-09-06 18:54:56 +0900

    refactor

--- Assets/graphDrawControl.cs
+++ Assets/graphDrawControl.cs
@@ -2,12 +2,17 @@
 using System.Collections;
 using System.Collections.Generic; // for List<>
 
+/*
+ * v0.1 2015/09/06
+ *   - draw line over Panel
+ */ 
+
 public class graphDrawControl : MonoBehaviour {
 
 	private GameObject lineGroup; // for grouping
 	List<Vector2> my2DPoint;
 	public GameObject panel;
-	float accTime = 0.0f;
+	public Canvas myCanvas; // to obtain canvas.scale
 	
 	void DrawLine(List<Vector2> my2DVec, int startPos) {
 		List<Vector3> myPoint = new List<Vector3>();
@@ -41,73 +46,33 @@ public class graphDrawControl : MonoBehaviour {
 		lineGroup.transform.parent = panel.transform; // to belong to panel
 	}
 
-	public Canvas myCanvas;
-
-	Vector2 getPanelPosition(Transform panel) {
-		Vector2 offset;
-		RectTransform rect = panel.GetComponent(typeof(RectTransform)) as RectTransform;
-
-		offset.x = -rect.pivot.x * rect.rect.width;
-		offset.y = -rect.pivot.y * rect.rect.height;
-
-		return myCanvas.worldCamera.ScreenToWorldPoint (offset);
-	}
-
-	void Start () {
+	void drawBottomLeftToTopRight(){
 		Vector2 pointPos;
-		my2DPoint = new List<Vector2> ();
 
-		RectTransform canvasRect = myCanvas.GetComponent<RectTransform> ();
 		RectTransform panelRect = panel.GetComponent<RectTransform> ();
-
-//		Debug.Log ("panel transform:" + panel.transform.position.ToString ());
-//		Debug.Log ("panel rect:" + panelRect.rect.width.ToString () + " x " 
-//			+ panelRect.rect.height.ToString ());
-//		Debug.Log ("canvas sizeDelta:" + canvasRect.sizeDelta.ToString ());
-//		Debug.Log("panel transform localPosition:" + panel.transform.localPosition.ToString());
-
-		Vector2 canSca = canvasRect.localScale;
-
-		Debug.Log ("canvas scale:" + canvasRect.localScale.ToString ());
-
 		float width = panelRect.rect.width;
 		float height = panelRect.rect.height;
+		
+		RectTransform canvasRect = myCanvas.GetComponent<RectTransform> ();
 
-		Debug.Log ("pivot: " + panelRect.pivot.ToString ());
-
+		// Bottom Left
 		pointPos = panel.transform.position;
 		pointPos.x -= width * 0.5f * canvasRect.localScale.x;
 		pointPos.y -= height * 0.5f * canvasRect.localScale.y;
-//
-//		Vector2 worldPos = myCanvas.worldCamera.ScreenToWorldPoint (pointPos);
-//		Vector2 viewPos = myCanvas.worldCamera.ScreenToViewportPoint (pointPos);
-
-		if (false) {
-			pointPos = new Vector2 (-8.45f, -2.61f);
-			pointPos.x += panel.transform.position.x;
-			pointPos.y += panel.transform.position.y;
-		}
 		my2DPoint.Add (pointPos);
 
-		pointPos = new Vector2 (8.45f, 2.61f);
-		pointPos.x += panel.transform.position.x;
-		pointPos.y += panel.transform.position.y;
+		// Top Right
+		pointPos = panel.transform.position;
+		pointPos.x += width * 0.5f * canvasRect.localScale.x;
+		pointPos.y += height * 0.5f * canvasRect.localScale.y;
 		my2DPoint.Add (pointPos);
-
+		
 		drawGraph ();
 	}
-	
-	void Update () {
-		
-	}
 
-	void OnGUI() {
-		accTime += Time.deltaTime;
-		if (accTime < 1.0f) {
-			return;
-		}
+	void Start () {
+		my2DPoint = new List<Vector2> ();
 
-		accTime = 0.0f;
-//		drawGraph ();
+		drawBottomLeftToTopRight ();
 	}
 }
